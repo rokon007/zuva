f7e5796eaaee0bcea6992d5c51e6dd583c617394
rokon007 <rezaulkrm85@gmail.com>
2022-09-26 19:05:52 +0600

    ert

--- resources/views/layouts/inc/footer.blade.php
+++ resources/views/layouts/inc/footer.blade.php
@@ -1,37 +1,7 @@
  <!-- Site footer -->
     <footer class="footer-section">
         <div class="container">
-            <div class="footer-cta pt-2 pb-2">
-                <div class="row">
-                    <div class="col-xl-4 col-md-4 mb-30 d-flex justify-content-center border-right border-dark">
-                        <div class="single-cta">
-                            <i class="fas fa-map-marker-alt"></i>
-                            <div class="cta-text">
-                                <h4>Find us</h4>
-                                <span><a href = "" style="color:#ffffff;font-size:14px">www.zuvaacademy.com</a></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-4 col-md-4 mb-30 d-flex justify-content-center border-right border-dark">
-                        <div class="single-cta">
-                            <i class="fas fa-phone"></i>
-                            <div class="cta-text">
-                                <h4>WhatsApp</h4>
-                                <span ><a href="https://wa.me/message/6JWZWJBN4VDEA1" target="_blank" style="color:#ffffff;font-size:14px">Send Message</a></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-4 col-md-4 mb-30 d-flex justify-content-center">
-                        <div class="single-cta">
-                            <i class="far fa-envelope-open"></i>
-                            <div class="cta-text">
-                                <h4>Mail us</h4>
-                                <span><a href = "mailto: info@zuvaacademy.com" style="color:#ffffff;font-size:14px">info@zuvaacademy.com</a></span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            
             <div class="footer-content pt-2 pb-2">
                 <div class="row">
                     <div class="col-xl-4 col-lg-4 mb-50 d-flex justify-content-center">
