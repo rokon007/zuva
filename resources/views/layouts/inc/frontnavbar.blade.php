090c748ab1b286f42c7b79a2b6fa51fe9d19bf8a
rokon007 <rezaulkrm85@gmail.com>
2022-09-27 19:34:22 +0600

    wer

--- resources/views/layouts/inc/frontnavbar.blade.php
+++ resources/views/layouts/inc/frontnavbar.blade.php
@@ -34,9 +34,20 @@
 </style>
 <style>
 a.ex1:hover {
- background-color: #008000; //add this of course....
-    color: #white;
+ background-color: #008000; 
+ color: #ffffff;
 }
+
+a.ex1 {
+  
+  
+  color: #666699;
+  font-family:Times New Roman;
+  font-size: 18px;
+  text-decoration: none;
+  
+}
+ 
  
 
 </style>
@@ -45,7 +56,7 @@ a.ex1:hover {
 <!--<nav class="navbar navbar-expand-lg navbar-dark bg-dark">-->
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container">
-    <a class="navbar-brand" href="{{url('/')}}">
+    <a class="navbar-brand" style=" color: #328fa8;font-family:Times New Roman;font-size: 25px;font-weight: bold;" href="{{url('/')}}">
 	  <img src="{{asset('frontend/image/logo1.png')}}" alt="Zuva Academy" width="60" height="60"> Zuva Academy
 	</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -57,10 +68,10 @@ a.ex1:hover {
           <a class="nav-link ex1" aria-current="page" href="{{ url('/') }}">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link ex1" href="{{ url('/about') }}">Why Choose Zuva Academy</a>
+          <a class="nav-link ex1"  href="{{ url('/about') }}">Why Choose Zuva Academy</a>
         </li>
 		<li class="nav-item">
-          <a class="nav-link ex1" href="{{ url('/team') }}">Our Staff</a>
+          <a class="nav-link ex1"  href="{{ url('/team') }}">Our Staff</a>
         </li>
 		<li class="nav-item">
           <a class="nav-link ex1" href="{{ url('/contact') }}">Curriculum</a>
@@ -72,15 +83,15 @@ a.ex1:hover {
         @if (Route::has('login'))
          @auth
          <li class="nav-item ">
-               <a class="nav-link ex1" href="{{ url('/dassboard') }}">Dassboard</a>
+               <a class="nav-link ex1"  href="{{ url('/dassboard') }}">Dassboard</a>
          </li>
           @else
     <!--    <li class="nav-item">
-          <a class="nav-link ex1" href="{{ route('login') }}">Log in</a>
+          <a class="nav-link ex1"  href="{{ route('login') }}">Log in</a>
         </li> -->
              @if (Route::has('register'))
           <!--   <li class="nav-item">
-               <a class="nav-link ex1" href="{{ route('register') }}">Register</a>
+               <a class="nav-link ex1"  href="{{ route('register') }}">Register</a>
              </li> -->
              @endif
          @endauth
